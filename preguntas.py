a6ecb221f92ef4c86cecc78aae70b7076e11bda7
daniel molina <hdpuentesm@correo.udistrital.edu.co>
2022-09-16 16:49:20 -0500

    commits 1

---
+++ preguntas.py
@@ -0,0 +1,21 @@
+import random
+class cuestionador:
+
+    def __init__(self):
+    
+        self.questions= [
+            "¿que es el almicantarat?",
+            "¿donde se encuentra el zenit?",
+            "¿cuántos órdemes ecisten en la taxonimia de suelos?"
+        ]
+        self.answers=  [
+            "circulo imaginario en esfera celeste",
+            "90 grados respecto al horizonte",
+            12 
+        ]
+
+    def jugar (self):
+        #genrar un numero aleatorio entre 0 y n-1, siendo n el tamaño de la lista de preguntas 
+        n=len(self.questions)
+        number= random.randint(0, n-1)
+        print(self.questions[number])
